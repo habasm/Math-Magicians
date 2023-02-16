ace1b1802fea1b7cfd4439957974deb0d4c30442
habasm <habasm365@gmail.com>
2023-02-16 14:30:16 +0300

    Math Magicians: refactor with hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import './scss/App.scss';
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <>
-        <BrowserRouter>
-          <Calculator />
-          <Routes>
-            <Route path="/calculator" element={<Calculator />} />
-          </Routes>
-        </BrowserRouter>
-      </>
-    );
-  }
+export default function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Calculator />
+        <Routes>
+          <Route path="/calculator" element={<Calculator />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
