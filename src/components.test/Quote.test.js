a1df6a2e9bbd43b8d48426b9317d96f190053b07
habasm <habasm365@gmail.com>
2023-02-24 16:30:42 +0300

    fireevent and screen react testings added with linter errors fixation

--- src/components.test/Quote.test.js
+++ src/components.test/Quote.test.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import {render, screen, fireEvent} from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import Quote from '../components/Quote';
-
-const Button = ({onClick, children}) => (
-  <button onClick={onClick}>{children}</button>
-)
 
 describe('Check if it', () => {
   it('renders Quote component', () => {
     render(<Quote />);
   });
-  const handleClick = jest.fn()
-  render(<Button onClick={handleClick}>Click Me</Button>)
-  fireEvent.click(screen.getByText(/click me/i))
-  expect(handleClick).toHaveBeenCalledTimes(1)
+  const handleClick = jest.fn();
+  render(<Button onClick={handleClick}>Click Me</Button>);
+  fireEvent.click(screen.getByText(/click me/i));
+  expect(handleClick).toHaveBeenCalledTimes(1);
 });
+
+const Button = ({ onClick, children }) => (
+  <button type="button" onClick={onClick}>{children}</button>
+);
 
 it('renders correctly when there are no items', () => {
   const tree = renderer.create(<Quote />).toJSON();
