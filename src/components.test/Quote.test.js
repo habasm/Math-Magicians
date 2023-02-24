69d70a6ae5964813c85605acec94c9a7614376ec
habasm <habasm365@gmail.com>
2023-02-24 16:17:45 +0300

    fireevent and screen react testings added

--- src/components.test/Quote.test.js
+++ src/components.test/Quote.test.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {render, screen, fireEvent} from '@testing-library/react'
 import renderer from 'react-test-renderer';
 import Quote from '../components/Quote';
+
+const Button = ({onClick, children}) => (
+  <button onClick={onClick}>{children}</button>
+)
 
 describe('Check if it', () => {
   it('renders Quote component', () => {
     render(<Quote />);
   });
+  const handleClick = jest.fn()
+  render(<Button onClick={handleClick}>Click Me</Button>)
+  fireEvent.click(screen.getByText(/click me/i))
+  expect(handleClick).toHaveBeenCalledTimes(1)
 });
 
 it('renders correctly when there are no items', () => {
